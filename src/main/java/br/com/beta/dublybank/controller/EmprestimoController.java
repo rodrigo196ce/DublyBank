ad82c59369d75c5eccb5736f8cf05fffc4f9c8d8
Rodrigo Souza <rodrigo@MacBook-Air-de-Rodrigo.local>
2022-07-12 09:11:56 -0300

    Modificando o Controller Empréstimo

--- src/main/java/br/com/beta/dublybank/controller/EmprestimoController.java
+++ src/main/java/br/com/beta/dublybank/controller/EmprestimoController.java
@@ -18,43 +18,5 @@ import java.time.LocalDate;
 @RequestMapping("emprestimo")
 public class EmprestimoController {
 
-    @Autowired
-    DublyUtil dublyUtil;
-
-    @Autowired
-    private ContaService contaService;
-
-    @Autowired
-    private UserService userService;
-
-    @RequestMapping
-    public String emprestimo(){
-      return"emprestimo/emprestimoHome.html";
-    }
-
-    @RequestMapping("telaSimular")
-    public String solicitar(SimularEmprestimoDto simularEmprestimoDto){
-        return "emprestimo/telaSimular.html";
-    }
-
-    @RequestMapping("simularSolicitacao")
-    public String simularSolicitacao(@Valid SimularEmprestimoDto simularEmprestimoDto, BindingResult result, Model model){
-        if(result.hasErrors()){
-            return "emprestimo/telaSimular.html";
-        }
-        Boolean resultValidDate = this.dublyUtil.validarDataBefore(simularEmprestimoDto.getData());
-        if(resultValidDate){
-            model.addAttribute("errorDate","A data não pode ser inferior a data atual.");
-            return "emprestimo/telaSolicitar.html";
-        }
-        Emprestimo emprestimo = this.contaService.emprestimo(
-                this.userService.findUserAndConta(),
-                simularEmprestimoDto.getValor(),
-                simularEmprestimoDto.getData());
-
-        model.addAttribute("emprestimo",emprestimo);
-        model.addAttribute("dataSolicitacao", LocalDate.now());
-        return "emprestimo/resultadoSimulacao.html";
-    }
 
 }
