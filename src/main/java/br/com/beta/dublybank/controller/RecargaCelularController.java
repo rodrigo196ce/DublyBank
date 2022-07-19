14b584207a025499b625a7a434ddf3ca652e1464
Rodrigo Souza <rodrigo@MacBook-Air-de-Rodrigo.local>
2022-07-19 09:22:18 -0300

    Criacao do Controller ''RecargaCelularController'

---
+++ src/main/java/br/com/beta/dublybank/controller/RecargaCelularController.java
@@ -0,0 +1,15 @@
+package br.com.beta.dublybank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("recargaCelular")
+public class RecargaCelularController {
+
+    @RequestMapping()
+    public String recargaCelular(){
+        return null;
+    }
+
+}
